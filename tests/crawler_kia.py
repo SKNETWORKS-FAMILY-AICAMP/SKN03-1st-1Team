09e15efdf56875c1ec8556f24ebb3d42e29e88c1
Heesun <goddns0272@gmail.com>
2024-07-24 10:47:56 +0900

    test: crawler_kia.py 수정

--- tests/crawler_kia.py
+++ tests/crawler_kia.py
@@ -18,39 +18,36 @@ driver.implicitly_wait(3)
 # driver.maximize_window()
 # driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
 
-elements = driver.find_elements(By.CLASS_NAME, value="cp-faq__accordion-item")
+questions = driver.find_elements(By.CLASS_NAME, value="accordion-title")
 
 faq_data = {}
-for elem in elements:
+for question in questions:
     try:
-        # # 탭의 텍스트를 확인하여 '기타'까지 가져오기
-        # category = elem.text.strip()
-        # action = ActionChains(driver)
-        # action.move_to_element(elem).click().perform()
-        # print(f"Processing category: {category}")
-
-        # 스크롤 조정
-        driver.execute_script("arguments[0].scrollIntoView();", elem)
-        
-        # # 대기 시간 추가하여 내용이 로드될 시간 확보
-        # WebDriverWait(driver, 30).until(
-        #     EC.presence_of_element_located((By.CLASS_NAME, 'faq-list'))
-        # )
-        
+        # 대기 시간 추가하여 내용이 로드될 시간 확보
+        WebDriverWait(driver, 20).until(
+            EC.presence_of_element_located((By.CLASS_NAME, 'accordion-label'))
+        )
         # 해당 카테고리의 모든 질문-답변 가져오기
         category = driver.find_element(By.CLASS_NAME, 'accordion-label')
-        questions = driver.find_element(By.CLASS_NAME, 'accordion-title')
-        answers = driver.find_element(By.CLASS_NAME, 'accordion-panel-inner')
-        print(category)
-        print(questions)
-        print(answers)
-        qa_pairs = []
-        for c, q, a in zip(category, questions, answers):
-            category_text = c.text.strip()
-            question_text = q.text.strip()
-            answer_text = a.text.strip()
-            qa_pairs.append({'question': question_text, 'answer': answer_text})
-            faq_data[category] = qa_pairs
+        # questions = driver.find_element(By.CLASS_NAME, 'accordion-title')
+
+        # 대기 시간 추가하여 내용이 로드될 시간 확보
+        WebDriverWait(driver, 20).until(
+            EC.presence_of_element_located((By.CLASS_NAME, 'accordion-panel-inner'))
+        )
+
+        answer = driver.find_element(By.CLASS_NAME, 'accordion-panel-inner')
+
+        print(category.text)
+        print(question.text)
+        print(answer.text)
+        # qa_pairs = []
+        # for c, q, a in zip(category, questions, answers):
+        #     category_text = c.text.strip()
+        #     question_text = q.text.strip()
+        #     answer_text = a.text.strip()
+        #     qa_pairs.append({'question': question_text, 'answer': answer_text})
+        #     faq_data[c] = qa_pairs
 
         # if category == "기타":
         #     break
