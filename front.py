8a96afebb0b53642e818fc6c793704b1b312f815
Song youngbin <dudqls574@naver.com>
2024-07-24 17:30:16 +0900

    test: search test 제작

---
+++ front.py
@@ -0,0 +1,4 @@
+import streamlit as st
+
+
+st.title(" 타이틀 입니다. ")
---
+++ tests/pages/streamlit_test.py
@@ -0,0 +1,5 @@
+import streamlit as st
+from datetime import datetime
+import pandas as pd
+
+st.page_link("./streamlit_search_test.py", label="search", icon="📖")
