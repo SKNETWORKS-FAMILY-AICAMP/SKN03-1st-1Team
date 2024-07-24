73d7f0760e2a6440407381a8e4a665eb498c5788
Heesun <goddns0272@gmail.com>
2024-07-24 12:29:26 +0900

    test: 제네시스 faq crawling test 완료

--- tests/faq_crowler_test.py
+++ tests/faq_crowler_test.py
@@ -4,6 +4,8 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import ElementClickInterceptedException, TimeoutException, StaleElementReferenceException
+
 
 driver = webdriver.Chrome()
 url = "https://www.genesis.com/kr/ko/support/faq.html?anchorID=faq_tab"
@@ -16,20 +18,24 @@ driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
 
 faq_data = []
 question_elements = driver.find_elements(By.CLASS_NAME, "cp-faq__accordion-item")
+total_num = len(question_elements)
 
-for element in question_elements:
-    driver.execute_script("arguments[0].scrollIntoView(true);", element)
-    driver.implicitly_wait(10)
-
+for num, element in enumerate(question_elements):
     answer_button = element.find_element(By.CLASS_NAME, "accordion-btn")
-    answer_button.click()
 
-    # category = element.find_element(By.CLASS_NAME, "accordion-label")
-    # question = element.find_element(By.CLASS_NAME, "accordion-title")
-    # answer = element.find_element(By.CLASS_NAME, "accordion-panel-inner")
-    # print(answer.text)
+    try:
+        answer_button.click()
+    except ElementClickInterceptedException:
+        driver.execute_script("arguments[0].scrollIntoView(true);", element)
+        driver.implicitly_wait(20)
+        answer_button.click()
     
-    # faq_data.append({"category": category.text, "question": question.text})
+    category = element.find_element(By.CLASS_NAME, "accordion-label")
+    question = element.find_element(By.CLASS_NAME, "accordion-title")
+    answer = element.find_element(By.CLASS_NAME, "accordion-panel-inner")
+    
+    faq_data.append({"category": category.text, "question": question.text, "answer": answer.text})
+    print(f"현재 {total_num} 중 {num+1}번째 데이터 수집 완료")
 
 driver.quit()
 
